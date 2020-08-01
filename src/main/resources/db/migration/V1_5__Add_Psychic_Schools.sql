5ec60aff7277324305fa8240e691323c9dc4791d
Troy Davidson <clandaith@gmail.com>
2020-07-31 22:08:04 -0600

    Added psychic schools

---
+++ src/main/resources/db/migration/V1_5__Add_Psychic_Schools.sql
@@ -0,0 +1,11 @@
+
+
+
+insert into psychic_schools (psychic_name, psychic_text) values ('School of Order', '');
+insert into psychic_schools (psychic_name, psychic_text) values ('School of Disorder', '');
+insert into psychic_schools (psychic_name, psychic_text) values ('School of Time', '');
+insert into psychic_schools (psychic_name, psychic_text) values ('School of Bio-Instinct', '');
+insert into psychic_schools (psychic_name, psychic_text) values ('Plasmids', '');
+insert into psychic_schools (psychic_name, psychic_text) values ('Psychic Block and Psychic Counter', '');
+
+
