66c9813f9870d3b36490970089bb4056e0bf5c69
Troy Davidson <clandaith@gmail.com>
2020-07-31 22:32:43 -0600

    add special rules

---
+++ src/main/resources/db/migration/V1_6__Add_Special_Rules.sql
@@ -0,0 +1,23 @@
+
+insert into special_rules (rule_name, level,  rule_text) values ('Inspiring Presence', 1, 'When a soldier with Inspiring Presence 1 executes a Cover Fire, Fire, or Advance Order during the Order Die Phase of a turn, all friendly soldiers within 3” of the soldier’s starting position—including the soldier as well—gain a +1 initiative modifier when executing ranged attacks until the end of the Order Dice Phase.');
+insert into special_rules (rule_name, level,  rule_text) values ('Inspiring Presence', 2, 'Soldier must first possess Inspiring Presence 1 to use this skill. Read as Inspiring Presence 1 but increase the range of this skill to 6”.');
+insert into special_rules (rule_name, level,  rule_text) values ('Inspiring Presence', 3, 'Soldier must first possess Inspiring Presence 2 to use this skill. Read as Inspiring Presence 2, but instead of gaining an initiative modifier for only ranged attacks, all soldiers gain a +1 bonus to their base initiative value until the end of the phase.');
+insert into special_rules (rule_name, level,  rule_text) values ('Natural Leader', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Natural Leader', 2, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Deadeye', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Deadeye', 2, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Weapons Mastery', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Fighting Instinct', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Fighting Instinct', 2, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Ruthless Nature', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Ruthless Nature', 2, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Terrifying Aura', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Psychic Prowess', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Psychic Prowess', 2, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Telekinetic Mastery', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Stealth', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Stealth', 2, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Field Medicine', 1, '');
+insert into special_rules (rule_name, level,  rule_text) values ('Field Mechanic', 2, 'When a soldier with Field Mechanic executes a Mend Order, they may transfer the effect of their Mend Order to any one friendly specialist marked (Mech) within 3” of their position.');
+insert into special_rules (rule_name, level,  rule_text) values ('Tough Skin', 1, 'Any time a soldier with Tough Skin must apply damage, the soldier temporarily gains +1 armor value when calculating the total damage against their armor.');
+
