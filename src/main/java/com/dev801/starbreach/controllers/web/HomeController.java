adffe416431dc35b8dbbcb1fc3df4e95bdc1c01a
Troy <clandaith@gmail.com>
2020-08-26 13:01:07 -0600

    fdgsdf

--- src/main/java/com/dev801/starbreach/controllers/web/HomeController.java
+++ src/main/java/com/dev801/starbreach/controllers/web/HomeController.java
@@ -1,5 +1,13 @@
 package com.dev801.starbreach.controllers.web;
 
+import java.util.List;
+
+import com.dev801.starbreach.entities.Faction;
+import com.dev801.starbreach.repositories.FactionRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +16,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+	@Autowired
+	FactionRepository factionRepository;
+
 	@GetMapping("/")
 	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
 			Model model) {
+		LOGGER.info("blah");
+
+		List<Faction> factions = factionRepository.findAll();
+
+		factions.stream().forEach(f -> LOGGER.info("Faction: {}", f));
+
+		model.addAttribute("factions", factions);
 		model.addAttribute("name", name);
 		return "index";
 	}
-
 }
