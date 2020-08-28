b1a95da5e6a5d853e6f0dcfaa4286c6f99fa2d08
Troy <clandaith@gmail.com>
2020-08-27 22:31:26 -0600

    asdfasdf

--- src/main/resources/static/starbreach.js
+++ src/main/resources/static/starbreach.js
@@ -1,48 +1,41 @@
 var soldierJSON = null;
+var relicsJSON = null;
+var specialRulesJSON = null;
 
 $(document).ready(function () {
-    $.getJSON("/api/soldier/", function (soldierData) {
-        //alert("Data Loaded: " + soldierData);
-
-        soldierJSON = soldierData;
+    $.getJSON("/api/soldier/", function (data) {
+        soldierJSON = data;
     });
 
-
     $("#soldier_add_button").click(function () {
-        var soldierName = $("#soldier_select option:selected").text();
-        var soldierId = $("#soldier_select option:selected").val();
-        // alert(soldierId + " " + soldierName);
-
-        blork(soldierId);
+        loadSoldierRow($("#soldier_select option:selected").val());
     });
 });
 
 function getSoldier(soldierId) {
-    var soldier1;
-    // soldierJSON.filter(soldier => soldier.id == soldierId)
-    //   .forEach(soldier => {console.log(soldier.id + " :: " + soldier.name); soldier1 = soldier;});
-
-    soldier1 = soldierJSON.filter(soldier => soldier.id == soldierId)
-        .reduce((x, soldier) => );
-
-    return soldier1;
+    return soldierJSON.filter(soldier => soldier.id == soldierId)
+        .reduce((x, soldier) => soldier, 0);
 }
 
-function blork(soldierId) {
-
+function loadSoldierRow(soldierId) {
     var soldier = getSoldier(soldierId);
+    var rowId = new Date().getTime();
 
     var newRowContent = "";
-    newRowContent += "<tr>";
-    newRowContent += "<td></td>";
-    newRowContent += "<td>" + soldier.name + "</td>";
-    newRowContent += "<td>" + soldier.move + "</td>";
-    newRowContent += "<td>" + soldier.initiative + "</td>";
-    newRowContent += "<td>" + soldier.health + "</td>";
-    newRowContent += "<td>" + soldier.armor + "</td>";
-    newRowContent += "<td>" + soldier.cost + "</td>";
-    newRowContent += "<td>X</td>";
+    newRowContent += "<tr id=" + rowId + ">";
+    newRowContent += "  <td></td>";
+    newRowContent += "  <td>" + soldier.name + "</td>";
+    newRowContent += "  <td>" + soldier.move + "</td>";
+    newRowContent += "  <td>" + soldier.initiative + "</td>";
+    newRowContent += "  <td>" + soldier.health + "</td>";
+    newRowContent += "  <td>" + soldier.armor + "</td>";
+    newRowContent += "  <td>" + soldier.cost + "</td>";
+    newRowContent += "  <td><i class=\"fas fa-backspace\" onclick=\"deleteSoldierRow(" + rowId + ")\"></i></td>";
     newRowContent += "</tr>";
 
     $("#soldier_table tbody").append(newRowContent);
+}
+
+function deleteSoldierRow(rowId) {
+    alert("Row id to delete: " + rowId);
 }
